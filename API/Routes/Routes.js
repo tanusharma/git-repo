923630c45ba4154bfa1c0c6a5cffa31560e99938
tanusharma <tanusharma8912@gmail.com>
2019-02-01 01:37:53 +0530

    chnages

--- API/Routes/Routes.js
+++ API/Routes/Routes.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 module.exports = function(app) {
-  var gameDataController = require('C:/Users/tanusharma2/API/Controllers/GameDataController.js');
+  var gameDataController = require('C:/Users/tanusharma2/Documents/GitHub/git-repo/API/Controllers/GameDataController.js');
 var apiRoutes =  express.Router();
 app.get('/',function(req,res){
     res.send('We are happy to see you using Chat Bot Webhook');
