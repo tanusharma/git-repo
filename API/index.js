1f8050db9933417e8b4f89445b3ccd806b13892f
tanusharma <tanusharma8912@gmail.com>
2019-02-01 02:12:36 +0530

    update index.js

--- API/index.js
+++ API/index.js
@@ -21,7 +21,11 @@ routes(server); //register the route
 var app = require('express')();
 
 
-var listener = server.listen(8000, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 8000
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+
 
