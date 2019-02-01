753afdf3335132c338abd8b60884512013c5d4db
tanusharma <tanusharma8912@gmail.com>
2019-02-01 15:33:08 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -21,4 +21,8 @@ routes(server); //register the route
 var app = require('express')();
 
 
-app.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+
